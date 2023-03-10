3b5dc8b9946b821157f755bdeafc8e7ce866eb6d
Aaron Jung <aaronjung3@gmail.com>
2023-03-10 09:45:20 -0800

    Some styling

--- frontend/src/components/RestaurantIndexItem/index.js
+++ frontend/src/components/RestaurantIndexItem/index.js
@@ -14,9 +14,10 @@ function RestaurantIndexItem({restaurant}) {
     return (
         <Link key={restaurant.id} to={`/restaurants/${restaurant.id}`}>
             <div>
-                {/* <img src={restaurant.photos[1].url} width="300" height="300"/> */}
+                <img src={restaurant.photos[1].url} width="300" height="300"/>
+
                 <h3>{restaurant.name}</h3>
-                <p>{restaurant.address}</p>
+                {/* <p>{restaurant.address}</p> */}
             </div>
         </Link>
     )
--- frontend/src/components/SplashIndex/index.js
+++ frontend/src/components/SplashIndex/index.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getRestaurants , fetchRestaurants } from '../../store/restaurants';
 import RestaurantIndexItem from '../RestaurantIndexItem';
+import "./SplashShow.css"
 
 function SplashIndex() {
     const dispatch = useDispatch();
@@ -16,9 +17,11 @@ function SplashIndex() {
 
     return (
         <>
-            <ul>
+        <div className="restaurant-cards-container">
+            <ul className="restaurant-card-div">
                 {restaurants.map(restaurant=> <RestaurantIndexItem key={restaurant.id} restaurant={restaurant}/>)}
             </ul>
+        </div>
         </>
     )
 }
