b45be4e0df7c58b88a08640977d70e995325b1db
Aaron Jung <aaronjung3@gmail.com>
2023-07-29 17:21:48 +0900

    define reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -12,7 +12,25 @@
 #  updated_at    :datetime         not null
 #
 class Reservation < ApplicationRecord
-    belongs_to :user
+    validate :unique_reservation_for_date_and_time
 
 
+
+    belongs_to :user,
+        foreign_key: :user_id,
+        class_name: :User
+
+    belongs_to :restaurant,
+        foreign_key: :restaurant_id,
+        class_name: :Restaurant
+
+    private
+
+    def unique_reservation_for_date_and_time
+        existing_reservation = Reservation.find_by(reservation_date: reservation_date, reservation_time: reservation_time)
+
+        if existing_reservation && (existing_reservation != self)
+        errors.add(:base, "This date and time is already reserved by another user.")
+        end
+    end
 end
